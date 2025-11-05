dca61bb6c695b56631fb85e30ae5cd64509249e3
swlee <swo98@naver.com>
2025-11-05 16:05:17 +0900

    add: base-time.entity 추가

---
+++ src/common/entities/base-time.entity.ts
@@ -0,0 +1,6 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export abstract class BaseTimeEntity {
+  @CreateDateColumn() createdAt: Date;
+  @UpdateDateColumn() updatedAt: Date;
+}
--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,7 +1,8 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseTimeEntity } from '../common/entities/base-time.entity';
 
 @Entity()
-export class Product {
+export class Product extends BaseTimeEntity {
   @PrimaryGeneratedColumn() id: number;
   @Column() name: string;
   @Column({ default: 0 }) stock: number;
