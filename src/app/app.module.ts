44377fcad0fcc61dc0fd996b94690d45984bd179
swlee <swo98@naver.com>
2025-11-05 15:52:17 +0900

    add: redis 동작 검증을 위한 ping-pong 보내기 모듈 추가, redis 컨테이너호스트포트 6666으로 변경

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LockModule } from '../lock/lock.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_DATABASE || 'postgres',
+      host: process.env.DB_HOST,
+      host: process.env.DB_HOST || 'localhost',                                                                                   │
+      port: parseInt(process.env.DB_PORT || '5432', 10),                                                                          │
+      username: process.env.DB_USERNAME || 'postgres',                                                                            │
+      password: process.env.DB_PASSWORD || '',                                                                                    │
+      database: process.env.DB_DATABASE || 'postgres',  
       autoLoadEntities: true,
       synchronize: true, // 개발 환경에서만 사용
     }),
     LockModule,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/redis/redis.service.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService {
+  constructor(@Inject('REDIS_CLIENT') private readonly redis: Redis) {}
+
+  async checkConnection(): Promise<string> {
+    try {
+      const result = await this.redis.ping();
+      return result; // 'PONG'
+    } catch (error) {
+      console.error(error);
+      return 'Error connecting to Redis';
+    }
+  }
+}
