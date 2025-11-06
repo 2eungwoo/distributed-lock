0cd0ff86ab2f2a9a9c6c6397ebc772e66790ed32
swlee <swo98@naver.com>
2025-11-06 20:56:36 +0900

    k6: k6스크립트를 http.batch()가 아니라 각각 독립된 실행으로 구성

---
+++ k6/3001.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 10,
+  iterations: 10,
+};
+
+export default function () {
+  const res = http.post(
+    'http://localhost:3001/product/5/deduct',
+    JSON.stringify({ quantity: 1 }),
+    {
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
+
+  check(res, {
+    'is status 201': (r) => r.status === 201,
+  });
+}
